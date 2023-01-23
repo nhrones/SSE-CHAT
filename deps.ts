a84a2bdad160c1e1471872822ca6b0a7e6653466
Nick Hrones <nhrones@users.noreply.github.com>
2023-01-22 22:47:49 -0500

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -2,3 +2,5 @@
 export * from "https://deno.land/std@0.173.0/http/server.ts"
 export * from "https://deno.land/std@0.173.0/path/mod.ts";
 export * from "https://deno.land/std@0.173.0/http/file_server.ts"
+
+
